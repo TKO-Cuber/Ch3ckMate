1935c258b7e3bf7407ec0c7c73407a88489c8005
TKO-Cuber <28767546+TKO-Cuber@users.noreply.github.com>
2018-02-01 10:41:17 -0500

    fix

--- Ch3ckMate.bat
+++ Ch3ckMate.bat
@@ -7,38 +7,51 @@
  ::The intro
  
  ::Get some needed stuff from user
+ SET emailFrom=ch3ckmatetss@gmail.com
+ SET body=Hey there! Some changes have occured in the TSS server! Go and check your script to see!
+ SET subject=TSS SERVER SIGNING CHANGED!
  set /p ECID=Please enter your ECID here ^>^>^>^>
  cls
  set /p DeviceModel=What is your device model? (iPhone7,2 for example) ^>^>^>^>
  cls
  set /p SendMail=Would you like to recieve an email if a previously unsigned version is signed again? (yes or no) ^>^>^>^>
-  IF "%SendMail%"=="yes" (
-    set /p SendMail= ^>^>^>^>
-) 
+  IF /I "%SendMail%"=="yes" (
+    set /p emailRecipient=What is your email address? ^>^>^>^>
+  )
  echo Thanks. Now just sit back, relax, and let the script run.
  
  :Start
  cd Programs
  
- ::Check iOS 11.2.5
- tsschecker_windows.exe -d %DeviceModel% --buildid 15D60 >output.txt
- cd Output
- >nul findstr /c:"Build 15D60 for device %DeviceModel% IS being signed!" output.txt && (
-    echo iOS 11.2.5 IS BEING SIGNED! SAVING BLOBS!
-	tsschecker_windows.exe -d %DeviceModel% --buildid 15D60 -e %ECID% -s >NUL
-      ) || (
-    echo.
-	cd ..
-  )
-  
  ::Check iOS 11.2.2
- tsschecker_windows.exe -d %DeviceModel% --buildid 15C202 >output1.txt
- cd Output
+ echo Checking iOS 11.2.2
+ tsschecker_windows.exe -d %DeviceModel% --buildid 15C202 >output.txt
  >nul findstr /c:"Build 15C202 for device %DeviceModel% IS being signed!" output1.txt && (
     echo iOS 11.2.2 IS BEING SIGNED! SAVING BLOBS!
-	tsschecker_windows.exe -d %DeviceModel% --buildid 15D60 -e %ECID% -s >NUL
+	tsschecker_windows.exe -d %DeviceModel% --buildid 15C202 -e %ECID% -s >NUL
+	::Send mail
+	cd Programs
+	cd blat
+	blat.exe -mailfrom %emailFrom% -to %emailRecipient% -body "%body%" -subject "%subj%" -server localhost
       ) || (
     echo.
-	cd ..
+	cd ../../..
   )
+  pause
   
+ ::Check iOS 11.2.5
+ echo Checking iOS 11.2.5
+ tsschecker_windows.exe -d %DeviceModel% --buildid 15D60 >output1.txt
+ cd Output
+ >nul findstr /c:"Build 15D60 for device %DeviceModel% IS being signed!" output1.txt && (
+    echo iOS 11.2.5 IS BEING SIGNED! SAVING BLOBS!
+	tsschecker_windows.exe -d %DeviceModel% --buildid 15D60 -e %ECID% -s >NUL
+	::Send mail
+	cd Programs
+	cd blat
+	blat.exe -mailfrom %emailFrom% -to %emailRecipient% -body "%body%" -subject "%subj%" -server localhost
+      ) || (
+    echo.
+	cd ../../..
+  )
+  pause
